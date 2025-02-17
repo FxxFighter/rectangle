5269524d6cf12135143547c51fa5111f14f58503
FxxFighter <xofighter24@gmail.com>
2025-02-18 00:14:41 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,31 +1,48 @@
 #include <iostream>
 using namespace std;
 
+// تعریف ساختار مستطیل
+struct Rectangle {
+    int width;
+    int height;
+
+    // تابع برای محاسبه مساحت
+    int area() {
+        return width * height;
+    }
+
+    // تابع برای محاسبه محیط
+    int perimeter() {
+        return 2 * (width + height);
+    }
+
+    // تابع برای چاپ شکل مستطیل
+    void printShape() {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                cout << "* ";
+            }
+            cout << endl;
+        }
+    }
+};
+
 int main() {
-    int width, height;
+    Rectangle rect;
 
     // دریافت ابعاد مستطیل از کاربر
     cout << "Enter the width of the rectangle: ";
-    cin >> width;
+    cin >> rect.width;
     cout << "Enter the height of the rectangle: ";
-    cin >> height;
-
-    // محاسبه مساحت و محیط
-    int area = width * height;
-    int perimeter = 2 * (width + height);
+    cin >> rect.height;
 
     // نمایش مساحت و محیط
-    cout << "Area of rectangle: " << area << endl;
-    cout << "Perimeter of rectangle: " << perimeter << endl;
+    cout << "Area of rectangle: " << rect.area() << endl;
+    cout << "Perimeter of rectangle: " << rect.perimeter() << endl;
 
     // چاپ شکل مستطیل
     cout << "Rectangle shape: " << endl;
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            cout << "* ";
-        }
-        cout << endl;
-    }
+    rect.printShape();
 
     return 0;
 }
